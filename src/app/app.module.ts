7620cecff1baa4bb7e6574861b5e1096c51ee5f1
GILSON RICARDO DE ALMEIDA <gilson.almeida@elotech.com.br>
2017-07-03 08:15:59 -0300

    criando projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { AngularFireDatabase } from 'angularfire2/database';
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
@@ -6,24 +7,42 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { AngularFireModule } from "angularfire2";
+import { ChatPage } from '../pages/chat/chat';
+
+export const firebaseConfig = {
+
+    apiKey: "AIzaSyDEoztwqOQ91PVvMXTndTLMOKbzrfvgeaI",
+    authDomain: "chat-5ec9d.firebaseapp.com",
+    databaseURL: "https://chat-5ec9d.firebaseio.com",
+    projectId: "chat-5ec9d",
+    storageBucket: "chat-5ec9d.appspot.com",
+    messagingSenderId: "689473812030"
+
+};
+
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    ChatPage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    ChatPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
+    AngularFireDatabase,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
